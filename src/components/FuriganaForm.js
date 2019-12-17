d715ee65082a580979361a6076dd938c7bdf2a2c
Risusagi <anastazja199707@gmail.com>
2019-12-17 19:28:50 +0100

    Text convertion handled
    Added convertion of text to furigana with creating new lines at places user created them. Deleted all unnecessary character and elements for romaji furigana

--- src/components/FuriganaForm.js
+++ src/components/FuriganaForm.js
@@ -6,11 +6,13 @@ import FuriganaTypeInput from './FuriganaTypeInput';
 class FuriganaForm extends React.Component {
     state = {
         text: '',
-        format: ''
+        // default value as in kuroshiro library ('to' option)
+        format: 'hiragana'
     }
-    
+
     handleChange = (e) => {
         const value = e.target.value;
+        // matches both format of furigana(hiragana, katakana, romaji) or text received from user
         const property = e.target.type === 'textarea' ? 'text' : 'format';
         
         this.setState(prevState => (
@@ -21,14 +23,23 @@ class FuriganaForm extends React.Component {
         ));
     }
 
-    // convert text to speech
+    // convert text to speech, uses Responsice voice library (applied by URL)
     handleTextReading = () => {
         responsiveVoice.speak(this.state.text, "Japanese Male", {rate: .9});
     }
 
+    handleTextConvertion = (e) => {
+        e.preventDefault();
+
+        // send data to parent
+        this.props.handleTextConvertion(this.state.text, this.state.format);
+    }
+
     render = () => {
         return (
-            <form>
+            <form
+                onSubmit={this.handleTextConvertion}
+            >
                 <ul>
                     <li>
                         <FuriganaTypeInput
--- src/components/ReadingAssistant.js
+++ src/components/ReadingAssistant.js
@@ -5,18 +5,81 @@ import KuromojiAnalyzer from "kuroshiro-analyzer-kuromoji";
 
 import FuriganaForm from './FuriganaForm';
 
-const ReadingAssistant = () => {
-    return (
-        <div>
-            <FuriganaForm />
+class ReadingAssistant extends React.Component {
+    state = {
+        convertedText: {__html: ''}
+    }
 
-            {/* output div */}
-            <div className="output-text">
+    // initialize kuroshiro converter
+    initializeKuroshiro = async () => {
+        this.kuroshiro = new Kuroshiro();
+        await this.kuroshiro.init(new KuromojiAnalyzer());        
+    }
 
+    componentDidMount = () => {
+        this.initializeKuroshiro();
+    }
+    
+    handleConvertion = async (text, format) => {
+        // if given text isn't in japanese don't tthis.handleNewLines to convert it
+        if (!Kuroshiro.Util.hasJapanese(text)) {
+            alert("Given text doesn't contain Japanese");
+            return;
+        }
+
+        const convertionResult = await this.kuroshiro.convert(text, {
+            to: format,
+            mode: 'furigana',
+            // will be applied only for convertions to romaji
+            romajiSystem: 'hepburn'
+        });
+        
+        
+        if (format === 'romaji') {
+            // romaji format creates only one ruby (default) that's why it is necessary to close it before the end of every paragraph
+            this.handleNewLines('</ruby></p> <p><ruby>', convertionResult);
+
+            // delete empty rt elements (with parentheses inside rp elements) to not show to users empty parentheses at the begininng of next pagaraphs if rt isn't supported by users' browsers
+            this.resultCode = this.resultCode.replace(/\<rp\>\(\<\/rp\>\<rt\>[\n\r]\<\/rt\>\<rp\>\)\<\/rp\>/g, '')
+
+            // replace all special symbols and numbers in furigana with spaces
+            // don't change to empty replacement (will create shift in furigana)
+                .replace(/<rp>\(<\/rp><rt>[0-9`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]<\/rt><rp>\)<\/rp>/g, '<rt> </rt>');
+        } else {
+            // hiragana and katakana create separated ruby for every kanji/kana character
+            this.handleNewLines('</p> <p>', convertionResult);
+        }
+
+        // change state to show text with furigana to user (html code, not normal text)
+        this.setState(prevState => (
+            {
+                ...prevState,
+                convertedText: {__html: `<p>${this.resultCode}</p>`}
+            }
+        ));
+    }
+
+    handleNewLines(newLineCode, codeToChange) {
+        this.resultCode = codeToChange.replace(/\n(?!<\/rt>)/g, newLineCode);
+    }
+
+    render = () => {
+        return (
+            <div>
+                <FuriganaForm
+                    handleTextConvertion={this.handleConvertion}
+                />
+
+                {/* output div */}
+                <div
+                    className="converted-text"
+                    dangerouslySetInnerHTML={this.state.convertedText}
+                >
+                </div>
+                
             </div>
-            
-        </div>
-    );
+        );
+    }
 };
 
 export default ReadingAssistant;
