3666a18d16ea0a250e362803624554324bc44e1d
Risusagi <anastazja199707@gmail.com>
2020-03-01 21:57:09 +0100

    Hide enviromental vars

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,3 @@
-const webpack = require('webpack');
-const dotenv = require('dotenv');
-
-const envKeys = () => {
-    const env = dotenv.config().parsed;
-    
-    return Object.keys(env).reduce((prev, next) => {
-        prev[`process.env.${next}`] = JSON.stringify(env[next]);
-        return prev;
-    }, {});
-};
-
 module.exports = {
     entry: './src/index.js',
     module: {
@@ -38,8 +26,5 @@ module.exports = {
                 loader: 'file-loader'
             }
         ]
-    },
-    plugins: [
-        new webpack.DefinePlugin(envKeys())
-    ]
+    }
 }
--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,16 @@ const common = require('./webpack.common');
 const merge = require('webpack-merge');
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const dotenv = require('dotenv');
+
+const envKeys = () => {
+    const env = dotenv.config().parsed;
+
+    return Object.keys(env).reduce((prev, next) => {
+        prev[`process.env.${next}`] = JSON.stringify(env[next]);
+        return prev;
+    }, {});
+};
 
 module.exports = merge(common, {
     mode: 'development',
@@ -27,7 +37,8 @@ module.exports = merge(common, {
         new HtmlWebpackPlugin({
             template: './public/index.html',
         }),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin(envKeys())
     ],
     devServer: {
         hot: true,
